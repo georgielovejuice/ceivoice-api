7b8898e3b5c757bcd9f21dd951dc6429f4b6471e
NukerDucker <np.intharasing@gmail.com>
2026-03-09 21:45:49 +0700

    refactor(prisma): remove comments about PrismaClient instantiation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-// Single shared PrismaClient instance for the entire application.
-// Instantiating multiple clients exhausts the connection pool.
 const prisma = new PrismaClient();
 
 export default prisma;
